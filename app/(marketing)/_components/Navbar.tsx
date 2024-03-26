4fad06822cfc976a2c501c1ea399303a824294c9
AreYouKhush <khushal9587@gmail.com>
2024-03-26 21:56:04 +0530

    01: landing page

---
+++ app/(marketing)/_components/Navbar.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useScrollTop } from "@/hooks/use-scroll-top";
+import { cn } from "@/lib/utils";
+import Logo from "./Logo";
+import { ModeToggle } from "@/components/mode-toggle";
+
+const Navbar = () => {
+  const scrolled = useScrollTop();
+  return (
+    <div
+      className={cn(
+        "z-50 bg-background fixed top-0 flex items-center p-6 w-full dark:bg-[#1F1F1F]",
+        scrolled && "border-b shadow-sm"
+      )}
+    >
+      <Logo></Logo>
+      <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        <div>
+          <ModeToggle></ModeToggle>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
