42eb258a9eb61eb5f2a05033ad4ed19e14c36468
AreYouKhush <khushal9587@gmail.com>
2024-03-31 04:15:01 +0530

    11: banner

--- app/(main)/_components/Navbar.tsx
+++ app/(main)/_components/Navbar.tsx
@@ -6,6 +6,8 @@ import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import { Title } from "./Title";
+import Banner from "./Banner";
+import { Menu } from "./Menu";
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -20,8 +22,11 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 
   if(document === undefined){
     return (
-        <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+        <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-4 w-full flex items-center justify-between">
             <Title.Skeleton/>
+            <div className="flex items-center gap-x-2">
+                <Menu.Skeleton/>
+            </div>
         </nav>
     )
   }
@@ -38,8 +43,14 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
             )}
             <div className="flex items-center justify-between w-full">
                 <Title initialData={document} />
+                <div className="flex items-center gap-x-2">
+                    <Menu documentId={document._id} />
+                </div>
             </div>
         </nav>
+        {document.isArchived && (
+            <Banner documentId={document._id} />
+        )}
     </>
   );
 };
